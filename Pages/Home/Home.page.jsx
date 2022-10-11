e8feee4a2bdcd3b1e00e53f930cc8b61e1ce07a8
LontchiFrank <fodjof6@gmail.com>
2022-10-11 12:32:52 +0100

    trying to push code

--- Pages/Home/Home.page.jsx
+++ Pages/Home/Home.page.jsx
@@ -4,11 +4,13 @@ import { View, Text, StyleSheet, Image } from "react-native";
 function Home(props) {
   return (
     <View style={styles.container}>
-      <Image source={require("../../img/1.png")} style={styles.img} />
-      <Text style={styles.detail}>
-        With an Updated motor, and integrated anti-theft tech the Frank scooters
-        are custom-tuned for the ultimate riding experience.
-      </Text>
+      <View style={styles.contain}>
+        <Image source={require("../../img/1.png")} style={styles.img} />
+        <Text style={styles.detail}>
+          With an Updated motor, and integrated anti-theft tech the Frank
+          scooters are custom-tuned for the ultimate riding experience.
+        </Text>
+      </View>
     </View>
   );
 }
@@ -17,18 +19,22 @@ export default Home;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    // flex: 1,
+    width: "100%",
+    height: "100%",
     backgroundColor: "white",
   },
+  contain: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
   img: {
-    height: "50%",
-    width: "120%",
+    height: "60%",
+    width: "90%",
     resizeMode: "contain",
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
-    borderBottomLeftRadius: 20,
+    borderTopRightRadius: 50,
+    borderBottomLeftRadius: 50,
   },
   detail: {
     color: "#000",
